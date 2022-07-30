7cbfaa60aa866c6ec4e2b4ed9c7ff77dc1f9a611
aabezgin <aabezgin@greenatom.ru>
2022-07-30 14:00:00 +0700

    add custom queue

--- queue.py
+++ queue.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class Queue:
     def __init__(self) -> None:
         self._list: list[int] = []
@@ -15,6 +18,46 @@ queue.push(7)
 queue.push(1)
 queue.push(6)
 
+print('Result of easy queue')
+print(queue.pop())
+print(queue.pop())
+print(queue.pop())
+print(queue.pop())
+
+
+class ListItem:
+    def __init__(self, value: int) -> None:
+        self.value: int = value
+        self.following: Optional["ListItem"] = None
+
+
+class CustomQueue:
+    def __init__(self) -> None:
+        self._root: Optional[ListItem] = None
+        self._head: Optional[ListItem] = None
+
+    def push(self, value: int) -> None:
+        if not self._root:
+            self._root = ListItem(value)
+            self._head = self._root
+        else:
+            new_item: ListItem = ListItem(value)
+            self._head.following = new_item
+            self._head = new_item
+
+    def pop(self) -> int:
+        value: int = self._root.value
+        self._root = self._root.following
+        return value
+
+
+queue = CustomQueue()
+queue.push(2)
+queue.push(7)
+queue.push(1)
+queue.push(6)
+
+print('Result of custom queue')
 print(queue.pop())
 print(queue.pop())
 print(queue.pop())
